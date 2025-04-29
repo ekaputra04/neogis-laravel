5aa2d69ee167dfcad96ba925bde8d2349c66cc6c
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-04-29 14:40:17 +0800

    feat: Implement CircleCategory resource with controller, requests, model, migration, factory, policy, and seeder

---
+++ app/Http/Controllers/CircleCategoryController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CircleCategory;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCircleCategoryRequest;
+use App\Http\Requests\UpdateCircleCategoryRequest;
+
+class CircleCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCircleCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CircleCategory $circleCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CircleCategory $circleCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCircleCategoryRequest $request, CircleCategory $circleCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CircleCategory $circleCategory)
+    {
+        //
+    }
+}
