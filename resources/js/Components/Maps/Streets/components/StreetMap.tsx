2c8f445e73ba280b5795964ae7472de3faf9cf9d
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-06-12 20:08:52 +0800

    feat: add start and end markers to street polylines in StreetMap component

--- resources/js/Components/Maps/Streets/components/StreetMap.tsx
+++ resources/js/Components/Maps/Streets/components/StreetMap.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, Polyline, Popup } from "react-leaflet";
+import { MapContainer, Marker, Polyline, Popup } from "react-leaflet";
 import { Button } from "@/Components/ui/button";
 import {
     AlertDialog,
@@ -89,96 +89,122 @@ export const StreetMap = memo(
                         weight = match ? match.weight : weight;
                     }
 
+                    const length = street.coordinates.length;
+
                     return (
-                        <Polyline
-                            key={street.id}
-                            positions={street.coordinates}
-                            color={color}
-                            weight={weight}
-                            dashArray={
-                                selectedStreet?.id == street.id
-                                    ? "10, 10"
-                                    : "0, 0"
-                            }
-                        >
-                            <Popup>
-                                <strong>
-                                    {street.nama_ruas || "Jalan Tanpa Nama"}
-                                </strong>
-                                <br />
-                                <br />
-                                {street.keterangan || "Tidak ada deskripsi"}
-                                <br />
-                                <br />
-                                Panjang: {roundToTwo(street.panjang) ||
-                                    "-"}{" "}
-                                meter
-                                <br />
-                                <br />
-                                Lebar: {roundToTwo(street.lebar) || "-"} meter
-                                <br />
-                                <br />
-                                Eksisting:{" "}
-                                {EksistingJalan.find(
-                                    (item) => item.id == street.eksisting_id
-                                )?.eksisting || "-"}
-                                <br />
-                                <br />
-                                Jenis:{" "}
-                                {JenisJalan.find(
-                                    (item) => item.id == street.jenisjalan_id
-                                )?.jenisjalan || "-"}
-                                <br />
-                                <br />
-                                Kondisi:{" "}
-                                {KondisiJalan.find(
-                                    (item) => item.id == street.kondisi_id
-                                )?.kondisi || "-"}
-                                <br />
-                                <br />
-                                <Button
-                                    className="mr-2"
-                                    onClick={() => {
-                                        toast.info("Processing request...");
-                                        router.visit(
-                                            `/dashboard/street/edit/${street.id}`
-                                        );
-                                    }}
-                                    variant={"outline"}
-                                >
-                                    Edit
-                                </Button>
-                                <AlertDialog>
-                                    <AlertDialogTrigger className="inline-flex justify-center items-center gap-2 bg-destructive hover:bg-destructive/90 disabled:opacity-50 shadow-sm px-3 py-1 rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring [&_svg]:size-4 font-medium text-destructive-foreground text-sm whitespace-nowrap transition-colors [&_svg]:pointer-events-none disabled:pointer-events-none [&_svg]:shrink-0">
-                                        Delete
-                                    </AlertDialogTrigger>
-                                    <AlertDialogContent>
-                                        <AlertDialogHeader>
-                                            <AlertDialogTitle>
-                                                Are you absolutely sure?
-                                            </AlertDialogTitle>
-                                            <AlertDialogDescription>
-                                                This action cannot be undone.
-                                                This will permanently delete
-                                                street from our servers.
-                                            </AlertDialogDescription>
-                                        </AlertDialogHeader>
-                                        <AlertDialogFooter>
-                                            <AlertDialogCancel>
-                                                Cancel
-                                            </AlertDialogCancel>
-                                            <AlertDialogAction
-                                                onClick={() =>
-                                                    onDelete(street.id)
-                                                }
-                                            >
-                                                Continue
-                                            </AlertDialogAction>
-                                        </AlertDialogFooter>
-                                    </AlertDialogContent>
-                                </AlertDialog>
-                            </Popup>
-                        </Polyline>
+                        <>
+                            <Marker
+                                position={[
+                                    street.coordinates[0][0],
+                                    street.coordinates[0][1],
+                                ]}
+                            >
+                                <Popup>Ini titik awal {street.nama_ruas}</Popup>
+                            </Marker>
+                            <Marker
+                                position={[
+                                    street.coordinates[length - 1][0],
+                                    street.coordinates[length - 1][1],
+                                ]}
+                            >
+                                <Popup>
+                                    Ini titik akhir {street.nama_ruas}
+                                </Popup>
+                            </Marker>
+                            <Polyline
+                                key={street.id}
+                                positions={street.coordinates}
+                                color={color}
+                                weight={weight}
+                                dashArray={
+                                    selectedStreet?.id == street.id
+                                        ? "10, 10"
+                                        : "0, 0"
+                                }
+                            >
+                                <Popup>
+                                    <strong>
+                                        {street.nama_ruas || "Jalan Tanpa Nama"}
+                                    </strong>
+                                    <br />
+                                    <br />
+                                    {street.keterangan || "Tidak ada deskripsi"}
+                                    <br />
+                                    <br />
+                                    Panjang: {roundToTwo(street.panjang) ||
+                                        "-"}{" "}
+                                    meter
+                                    <br />
+                                    <br />
+                                    Lebar: {roundToTwo(street.lebar) ||
+                                        "-"}{" "}
+                                    meter
+                                    <br />
+                                    <br />
+                                    Eksisting:{" "}
+                                    {EksistingJalan.find(
+                                        (item) => item.id == street.eksisting_id
+                                    )?.eksisting || "-"}
+                                    <br />
+                                    <br />
+                                    Jenis:{" "}
+                                    {JenisJalan.find(
+                                        (item) =>
+                                            item.id == street.jenisjalan_id
+                                    )?.jenisjalan || "-"}
+                                    <br />
+                                    <br />
+                                    Kondisi:{" "}
+                                    {KondisiJalan.find(
+                                        (item) => item.id == street.kondisi_id
+                                    )?.kondisi || "-"}
+                                    <br />
+                                    <br />
+                                    <Button
+                                        className="mr-2"
+                                        onClick={() => {
+                                            toast.info("Processing request...");
+                                            router.visit(
+                                                `/dashboard/street/edit/${street.id}`
+                                            );
+                                        }}
+                                        variant={"outline"}
+                                    >
+                                        Edit
+                                    </Button>
+                                    <AlertDialog>
+                                        <AlertDialogTrigger className="inline-flex justify-center items-center gap-2 bg-destructive hover:bg-destructive/90 disabled:opacity-50 shadow-sm px-3 py-1 rounded-md focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring [&_svg]:size-4 font-medium text-destructive-foreground text-sm whitespace-nowrap transition-colors [&_svg]:pointer-events-none disabled:pointer-events-none [&_svg]:shrink-0">
+                                            Delete
+                                        </AlertDialogTrigger>
+                                        <AlertDialogContent>
+                                            <AlertDialogHeader>
+                                                <AlertDialogTitle>
+                                                    Are you absolutely sure?
+                                                </AlertDialogTitle>
+                                                <AlertDialogDescription>
+                                                    This action cannot be
+                                                    undone. This will
+                                                    permanently delete street
+                                                    from our servers.
+                                                </AlertDialogDescription>
+                                            </AlertDialogHeader>
+                                            <AlertDialogFooter>
+                                                <AlertDialogCancel>
+                                                    Cancel
+                                                </AlertDialogCancel>
+                                                <AlertDialogAction
+                                                    onClick={() =>
+                                                        onDelete(street.id)
+                                                    }
+                                                >
+                                                    Continue
+                                                </AlertDialogAction>
+                                            </AlertDialogFooter>
+                                        </AlertDialogContent>
+                                    </AlertDialog>
+                                </Popup>
+                            </Polyline>
+                        </>
                     );
                 })}
                 <TemporaryMarker />
